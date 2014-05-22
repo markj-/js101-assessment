952fbc45a4971871f433430316fcb4f531f14b80
Mark Jones <mark.jones@kaldorgroup.com>
2014-05-22 13:04:00 +0100

    WIP

---
+++ main.js
@@ -0,0 +1,126 @@
+(function() {
+  'use strict';
+
+  /**
+  * 1. Strings
+  **/
+
+  /* 1.1 Create a variable called 'yourName' containing your name as a string */
+
+  /* 1.2 Create two variables, one called foo and one called bar. foo should contain a String equal to 'foo' and bar should contain a String equal to 'bar'. Concatenate the variables and store this in a variable called foobar. */
+
+  /* 1.3 Explain why concatenating a String with value '1' and a Number with value 1 is equal to a String with value '11' */
+
+  // ANSWER HERE:
+
+  /* 1.4 Create a variable called 'myString' as a String equal to 'abccefg'. Change the second c in this string to be a d (there are several ways to do this) */
+
+  /* 1.5 Log the data type that yourName contains to your console */
+
+  /* 1.6 console.log the index of the letter b in 'myString' */
+
+
+  /**
+  * 2. Numbers
+  **/
+
+  /* 2.1 Create a variable called 'yourAge' containing your age as a Number */
+
+  /* 2.2 Add two variables storing Numbers together */
+
+  /* 2.3 Subtract a variable storing a Number from another variable storing a Number */
+
+  /* 2.4 Multiply two variables storing Numbers together */
+
+  /* 2.5 Get the remainder when 10 / 3 */
+
+
+  /**
+  * 3. Truthy and falsey values
+  **/
+
+  /* 3.1 Which values are falsey in JavaScript? (there are 6 all together) */
+
+  // ANSWER HERE:
+
+  /* 3.2 Which values are truthy in JavaScript? */
+
+  // ANSWER HERE
+
+
+  /**
+  * 4. Arrays
+  **/
+
+  /* 4.1 Create a variable called 'memberList' containing the names of everyone in JS101 as an array */
+
+  /* 4.2 Add a new member to the end of your your 'memberList' */
+
+  /* 4.3 Remove the member at the end of your 'memberList' */
+
+  /* 4.4 Add a member to the front of your 'memberList' */
+
+  /* 4.5 Remove the member at the front of your 'memberList' */
+
+  /* 4.6 console.log the second member in your 'memberList' */
+
+  /* 4.7 Change the name of the first member in your 'memberList' */
+
+
+  /**
+  * 5. Objects
+  **/
+
+  /* 5.1 Create a variable called 'me' as an object containing two properties, 'name' and 'age' */
+
+  /* 5.2 Create a variable called 'members' as an array of objects for each member of JS101. Structure it like the object from task 4.1 */
+
+  /* 5.3 Add a new property of your choice to your 'me' Object */
+
+  /* 5.4 Delete the 'age' property of your 'me' Object
+
+  /* 5.5 Change the 'name' property of your 'me' Object
+
+  /* 5.6 console.log the 'name' property of your 'me' object
+
+
+  /**
+  * 6. Functions
+  **/
+
+
+  /**
+  * 7. Scope
+  **/
+
+  /* 7.1 Intentionally create a global variable called x. You should be able to console.log window.x and get the value of x if this is done correctly */
+
+  /* 7.2 Create an IIFE below and create one variable called 'inner' inside it with any value. Prove that this can only be accessed inside the IIFE using console.log. Why is this? */
+
+  // ANSWER WHY HERE:
+
+
+  /**
+  * 8. DOM
+  **/
+
+  // There is a div in index.html to use in this section
+
+  /* 8.1 Select the div using its id */
+
+  /* 8.2 Select the div using its tag name */
+
+  /* 8.3 Select the div using the 'foo' part its class name */
+
+  /* 8.4 Add a class of 'my-class' to the div */
+
+  /* 8.5 Remove the class 'pugpig' from the div */
+
+  /* Check if the div has a class of bar */
+
+
+  /**
+  * 9. Event Listeners
+  **/
+
+})();
