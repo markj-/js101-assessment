e6d17eb4ce2af4151cfb67ee477004db63ff4df5
Mark Jones <mark.jones@kaldorgroup.com>
2014-07-29 10:05:00 +0100

    Assessment 2

---
+++ main2.js
@@ -0,0 +1,75 @@
+(function() {
+  'use strict';
+
+  /**
+  * 1. Control Flow
+  **/
+
+  /* 1.1 Using if and else statements, log out the word true or false depending on the value of a variable being truthy or falsey */
+
+  /* 1.2 Using an if statement, log out the word 'logic' based on the value of two different variables, e.g. something like: both variables are true true, both variables are false false or one is true and one is false */
+
+  /* 1.3 Write an example using else if */
+
+  /* 1.4 Write a for loop that decrements (and logs) a variable containing the Number  100 until it is equal to 0 (so log the numbers 100 to 0, inclusive) */
+
+  /* 1.5 Write a while loop that decrements (and logs) a variable containing the Number 100 until it is equal to 50 (so log the numbers 100 to 50, inclusive) */
+
+
+  /**
+  * 2. Functions
+  */
+
+  /* 2.1 Write a function definition called logToConsole that accepts one parameter and logs it to the console when it is called */
+
+  /* 2.2 Call your logToConsole function passing a variable in as the parameter */
+
+  /* 2.3 Write a function definition called logAllToConsole that accepts any number of parameters and logs all of them one at a time to the console when it is called */
+
+  /* 2.4 Call your logAllToConsole function passing 4 different variable in as the parameters. Call it again using 3 parameters to show it can handle any amount */
+
+  /* 2.5 Write an anonymous function expression called talk that accepts one parameter and alerts the value of the parameter when it is called */
+
+  /* 2.6 Create an object called person that has a property called sayHello that is equal to an anonymous function expression. sayHello should accept one parameter called name and alerts "Hello, name" where name is equal to the name parameter, when it is called */
+
+  /* 2.7 Where in our functions should we declare variables to avoid the dangers of variable hoisting? */
+
+  // ANSWER HERE:
+
+  /* 2.8 Write a function that adds two numbers together and returns the total. Create a variable called total that is equal to the return value of the function */
+
+  /* 2.9 Demonstrate function scope */
+
+
+  /**
+  * 3. this
+  */
+
+  /* 3.1 What is 'this' equal to when it is evaluated in the global scope? */
+
+  // ANSWER HERE:
+
+  /* 3.2 What is 'this' equal to when it is evaluated in an event callback? */
+
+  // ANSWER HERE:
+
+  /* 3.3 What is 'this' equal to when it is evaluated in a function that is a property of an object. Demonstrate this in code by logging the value */
+
+  /* 3.4 Call a function in such a way that logging 'this' evaluates to an Object containing a property called levelledUp that is equal to the String "I know JS" */
+
+  /**
+  * 4. Events: Bonus points if you wait for a certain event before any of the next answers run
+  */
+
+  /* 4.1 When the div containing the words click me is clicked, change its background colour */
+
+  /* 4.2 When the div containing the words click me and delete is clicked, remove it from the DOM. Bonus points for managing memory with your answer */
+
+  /* 4.3 When you move your mouse over the div containing the words mouse move, console.log the event object */
+
+  /* 4.4 When you click the link, prevent it following the link */
+
+  /* 4.5 When you type into the input, make the span with class input-value contain the same text. Bonus points if it works on Firefox as well as Chrome */
+
+
+})();
